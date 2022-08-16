5b0ac4a30559ecff3b970d57676a0d4f5603a5ae
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2022-08-16 07:23:59 +0300

    introduction of clean code

---
+++ Introduction/introduction.js
@@ -0,0 +1,63 @@
+/*
+    *** Waht is Clean-Code ***
+        => It's not about whether code works or not, instead it's about whether code is esay to read and understand.
+        => Code should be readable and meaningful
+        => Code should reduce cognitive load, you have to go
+         through, so that we have less congnitive load, and 
+         undertanding is easy.
+        => Should be concise and 'to the point'
+        => Should avoid unintutive names, complex nesting and big code blocks.
+        => Should follow common best practice and patterns.
+        => Should be fun to write and to maintain.
+
+    Note: Clean code is easy to understand and Dirty code is not easy to understand.
+*/
+
+/*
+    *** Key Pain Points of the Clean code ***
+
+        Names: Variable, Functions, Classes
+        Structure & Comments: Code Formatting, Good and Bad Comments
+        Functions: Length, Parameters
+        Conditions and Error Handling: Deep Nesting, Missing Error Handling.
+        Classes and Data Structure: Missing Distinction, Bloated Classes
+
+    Above Solution: Rules and Concepts, Patterns and Principles, Text-Driven-Development.
+*/
+
+/*
+    *** Clean code does not require strong typing ***
+        => Types can help preventing error and can improve readability.
+        => But code can also 100% readable and meaningful without types.
+*/
+
+/*
+    *** Clean Code Vs Clean Architecture ***
+                    Clean Code
+            => How to write the code
+            => Focus on single problems / files
+
+                Clean Architecture 
+            => It about how structure our project
+            => How we seperate our entities
+            => How we store our data
+            => Focus on the project as a whole.
+            Note: where to write which code. Do we use dependency injection or not?
+
+    *** Clean Code Vs Pattern and Principles ***
+                    Clean Code
+            => Write code which is readable and esay to understand
+
+                    Patterns and Principles
+            => Write code which is maintainable and extensible.
+            => If you name things poorly, format things poorly,
+            functions which are super clunky and messed up and hard to read
+
+*/
+
+/*
+    *** The way of Clean Code ***
+    => We will always find ways of improving our code => As our project elvolves and changes, our code will need to change
+
+    Note: Question old code and refactor a lot!
+*/
