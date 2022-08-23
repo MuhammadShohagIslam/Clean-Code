4b9addf0f5f63ecb9431c2d8d0f13c99dc4e8c39
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2022-08-23 13:54:33 +0300

    Done Name Casing

---
+++ Naming/name-casing.js
@@ -0,0 +1,16 @@
+/*
+    *** Name Casing ***
+
+            * There are Four Name Casing to write our Name *
+
+    sanke-case          camelCase               PascalCase          kebab-case
+    is_valid            isValid                 IsValid             is-valid
+    send_response       sendResponse            SendResponse        send-response
+
+    e.g Python          e.g JavaScript,Java     e.g Python,Java     e.g HTML 
+                                                JavaScript,
+    Varables,           Variables          
+    functions,          functions               Classes             Custome HTML Elements
+    methods             methods
+
+*/
