f3c0dd875d48514cf2cf7075b4e9f90aa39dd1ba
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2022-08-26 05:59:42 +0300

    Done Introduction

---
+++ Functions-Methods/introduction.js
@@ -0,0 +1,7 @@
+/*
+    *** Functions-Methods ***
+        => Calling the function should be readable.
+        => The argument order should be clear
+        => Working with the function should be easy / readale.So the function body, the definition of a function should be readable
+        => The length of the function body matters.
+*/
